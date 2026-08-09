191c822aa4441869e9dcd7e73580a0ca4cb0411c
Dvd-Znf <email@dvdznf.xyz>
2026-08-09 19:04:36 +0300

    refact: move out non-user related options from user module

--- color.nix
+++ color.nix
@@ -15,18 +15,4 @@
 
   users.extraUsers.color.extraGroups = [ "audio" ];
 
-  programs.bash.promptInit = ''
-    PS1='\[\033[01;94m\][\u@\h\[\033[01;37m\] \W\[\033[01;94m\]]\$\[\033[00m\] '
-  '';
-
-  environment.shellAliases = {
-    icat = "kitty +kitten icat";
-    ll = "ls -la";
-    la = "ls -a";
-    ssh = "kitten ssh";
-    c = "sudo nixos-rebuild switch --flake './#laptop'";
-    cu = "sudo nix flake update";
-    cm = "git commit -am \"flake: update\"";
-    ",," = "nix-shell -p ";
-  };
 }
